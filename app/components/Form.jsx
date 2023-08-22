378a05509ae4a7d293fd44d3efb1ed254011d232
azubair2012 <studio.zubair@gamil.com>
2023-08-22 16:45:58 +0600

    contact form to email added

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,29 +1,62 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+  //SDK start
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qfpmymr",
+        "template_62nsl8h",
+        form.current,
+        "u4wmW8gGyno08287R"
+      )
+      .then(
+        (result) => {
+          document.getElementById("success").innerHTML =
+            "Your Message Was Sent, Thank You! We Will Be In Touch Shortly";
+          console.log(result.text);
+        },
+        (error) => {
+          document.getElementById("success").innerHTML =
+            "Sorry your message was not sent! Please try email or WhatsApp";
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
+  };
+  //SDK end
   return (
     <div>
-      <form action="" className="flex flex-col items-center">
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="flex flex-col items-center"
+      >
         <div className="flex flex-col font-bold text-lg">
           <label htmlFor="">Name</label>
           <input
             required
             type="text"
-            name="name"
+            name="user_name"
             className="w-[350px] px-2 py-2 border-2 border-gray-800 rounded-full"
           />
         </div>
         <div className="flex flex-col font-bold text-lg">
-          <label htmlFor="">Email</label>
+          <label>Email</label>
           <input
             required
-            type="text"
-            name="email"
+            type="email"
+            name="user_email"
             className="w-[350px] px-2 py-2 border-2 border-gray-800 rounded-full"
           />
         </div>
         <div className="flex flex-col font-bold text-lg">
-          <label htmlFor="">Message</label>
+          <label>Message</label>
           <input
             required
             type="text"
@@ -33,12 +66,13 @@ const Form = () => {
         </div>
         <button
           type="submit"
-          value="Submit"
+          value="Send"
           className="font-title mt-7 bg-gray-800 text-[20px] text-white rounded-[4px] w-[120px] h-10 flex items-center justify-center transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200"
         >
           Send Message
         </button>
       </form>
+      <div className="text-[25px] mt-2 px-2 text-center" id="success"></div>
     </div>
   );
 };
